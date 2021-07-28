cc0257467aa5232e60bf8cbdbba188816381d901
JY-5 <58899117+JY-5@users.noreply.github.com>
2021-07-27 19:39:17 -0700

    debug

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 import json
+import sys
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
@@ -13,6 +14,7 @@ def get_list():
     data = dataJsonFile.read()
     result = json.loads(data)
     print(result)
+    sys.stdout.flush()
     return jsonify(result)
 
 @app.route('/')
